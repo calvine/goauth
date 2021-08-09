942d900e8a329957db5bd4e4522559c08ee6c3aa
calvine <calvin.echols@gmail.com>
2021-08-08 23:44:40 -0400

    Error generator work. Almost done!

--- core/errors/generator/main.go
+++ core/errors/generator/main.go
@@ -4,75 +4,116 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"go/format"
+	"io/ioutil"
 	"os"
 	"strings"
 	"text/template"
 
+	"github.com/calvine/goauth/core/errors/generator/templates"
 	"github.com/calvine/goauth/core/utilities"
 )
 
+const (
+	errorsDir = "../"
+	codesDir  = "../codes/"
+)
+
+type dataItem struct {
+	Name     string `json:"name"`
+	DataType string `json:"dataType"`
+}
+
 type errorData struct {
 	// Code is expected to be Pascal Case
-	Code     string
-	Message  string
-	MetaData []string
+	Code     string     `json:"code"`
+	Message  string     `json:"message"`
+	MetaData []dataItem `json:"metaData"`
+}
+
+type jsonErrorFileData struct {
+	Data []errorData
+}
+
+type options struct {
+	emitOutputFiles bool
+	jsonErrorData   string
+	errorFilePath   string
 }
 
 func main() {
-	example := errorData{
-		Code:    "NoUserIdFound",
-		Message: "failed to get item by user id",
-		MetaData: []string{
-			"userId",
-		},
-	}
+	genOptions := options{}
+	flag.BoolVar(&genOptions.emitOutputFiles, "emitOutputFiles", false, "if set source files will be created for the error.")
+	flag.StringVar(&genOptions.jsonErrorData, "jsonErrorData", "", "a json string that represents the error details to generate code for.")
+	flag.StringVar(&genOptions.errorFilePath, "errorFilePath", "", "The path the a json file that contains an array of error objects to have code generated for.")
+	flag.Parse()
+	fmt.Printf("%v\n\n", genOptions)
 	funcMap := template.FuncMap{
 		"ToUpper":            strings.ToUpper,
 		"ToLower":            strings.ToLower,
 		"UpperCaseFirstChar": utilities.UpperCaseFirstChar,
 		"LowerCaseFirstChar": utilities.LowerCaseFirstChar,
 	}
-	errConstructorTemplate := template.Must(template.New("Error constructor template").Parse(errorConstructorTemplate)).Funcs(funcMap)
-	errCodeTemplate := template.Must(template.New("Error code template").Parse(errorCodeTemplate)).Funcs(funcMap)
-	constructorBuffer := bytes.NewBufferString("")
-	codeBuffer := bytes.NewBufferString("")
-	errConstructorTemplate.Execute(constructorBuffer, example)
-	errCodeTemplate.Execute(codeBuffer, example)
-
-	errConstructorCode, _ := format.Source([]byte(constructorBuffer.String()))
-	errCodeCode, _ := format.Source([]byte(codeBuffer.String()))
-
-	fmt.Fprint(os.Stdout, string(errConstructorCode))
-	fmt.Fprint(os.Stdout, string(errCodeCode))
-}
-
-const errorConstructorTemplate = `
-package errors
-
-import (
-	"fmt"
-
-	"github.com/calvine/goauth/core/errors/codes"
-)
-
-// New{{ .Code }}Error creates a new specific error
-func New{{ .Code }}Error({{ range .MetaData }}{{ . }} interface{},{{ end }} includeStack bool) RichError {
-	msg := "{{ .Message }}"
-	err := NewRichError(codes.ErrCode{{ .Code }}, msg){{ range .MetaData }}.AddMetaData("{{ . }}", {{ . }}){{ end }}
-	if includeStack {
-		err = err.WithStack(1)
+	errConstructorTemplate := template.Must(template.New("Error constructor template").Parse(templates.ErrorConstructorTemplate)).Funcs(funcMap)
+	errCodeTemplate := template.Must(template.New("Error code template").Parse(templates.ErrorCodeTemplate)).Funcs(funcMap)
+	errDataSlice := make([]errorData, 0)
+	if genOptions.errorFilePath != "" {
+		jsonErrorDataFileData, err := ioutil.ReadFile(genOptions.errorFilePath)
+		if err != nil {
+			errMsg := fmt.Sprintf("failed to open file %s - %s", genOptions.errorFilePath, err.Error())
+			panic(errMsg)
+		}
+		json.Unmarshal(jsonErrorDataFileData, &errDataSlice)
+	} else if genOptions.jsonErrorData != "" {
+		var errData errorData
+		err := json.Unmarshal([]byte(genOptions.jsonErrorData), &errData)
+		if err != nil {
+			// handle bad json data...
+			errMsg := fmt.Sprintf("failed to unmarshal json data: %s", err.Error())
+			panic(errMsg)
+		}
+		errDataSlice = append(errDataSlice, errData)
+	} else {
+		panic("jsonErrorData or errorFilePath are required.")
 	}
-	return err
+	fmt.Printf("%v\n\n", errDataSlice)
+	for _, data := range errDataSlice {
+		constructorBuffer := bytes.NewBufferString("")
+		err := errConstructorTemplate.Execute(constructorBuffer, data)
+		if err != nil {
+			fmt.Printf("failed to execute error constructor template: %s", err.Error())
+			continue
+		}
+		errConstructorCode, err := format.Source([]byte(constructorBuffer.String()))
+		if err != nil {
+			fmt.Printf("%s", constructorBuffer)
+			fmt.Printf("Failed to run format.Source on error code template: %s", err.Error())
+			continue
+		}
+
+		codeBuffer := bytes.NewBufferString("")
+		err = errCodeTemplate.Execute(codeBuffer, data)
+		if err != nil {
+			fmt.Printf("failed to execute error code template: %s", err.Error())
+			continue
+		}
+		errCodeCode, err := format.Source([]byte(codeBuffer.String()))
+		if err != nil {
+			fmt.Printf("%s", codeBuffer)
+			fmt.Printf("Failed to run format.Source on error code template: %s", err.Error())
+			continue
+		}
+
+		if !genOptions.emitOutputFiles {
+			fmt.Printf("\n\n************** Error Constructor Code **************\n\n")
+			fmt.Fprint(os.Stdout, string(errConstructorCode))
+			fmt.Printf("\n\n************** Error Code Code **************\n\n")
+			fmt.Fprint(os.Stdout, string(errCodeCode))
+		} else {
+			// emit files...
+		}
+	}
 }
-
-`
-
-const errorCodeTemplate = `
-package codes
-
-// ErrCode{{ .Code }} {{ .Message }}
-const ErrCode{{ .Code }} = "{{ .Code }}"
-
-`
---
+++ core/errors/generator/templates/templates.go
@@ -0,0 +1,36 @@
+package templates
+
+const (
+	ErrorConstructorTemplate = `
+package errors
+
+/* WARNING: This is GENERATED CODE Please do not edit. */
+
+import (
+	"fmt"
+
+	"github.com/calvine/goauth/core/errors/codes"
+)
+
+// New{{ .Code }}Error creates a new specific error
+func New{{ .Code }}Error({{ range .MetaData }}{{ .Name }} {{ .DataType }}, {{ end }}includeStack bool) RichError {
+	msg := "{{ .Message }}"
+	err := NewRichError(codes.ErrCode{{ .Code }}, msg){{ range .MetaData }}.AddMetaData("{{ .Name }}", {{ .Name }}){{ end }}
+	if includeStack {
+		err = err.WithStack(1)
+	}
+	return err
+}
+
+`
+
+	ErrorCodeTemplate = `
+package codes
+
+/* WARNING: This is GENERATED CODE Please do not edit. */
+
+// ErrCode{{ .Code }} {{ .Message }}
+const ErrCode{{ .Code }} = "{{ .Code }}"
+
+`
+)
