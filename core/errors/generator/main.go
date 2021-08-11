c41363a5cfcd9f95ef67977d8f5fc64721afa69a
calvine <calvin.echols@gmail.com>
2021-08-10 22:10:02 -0400

    documenting error properties for error generator

--- core/errors/generator/main.go
+++ core/errors/generator/main.go
@@ -27,16 +27,21 @@ const (
 )
 
 type dataItem struct {
-	Name     string `json:"name"`
+	// Name is the name of the parameter added to the error constructor as well as the label added to the parameter in the errors metadata.
+	Name string `json:"name"`
+	// DataType is a string that tells the go generator what the type of this field is for the error constructor.
 	DataType string `json:"dataType"`
 }
 
 type errorData struct {
-	// Code is expected to be Pascal Case
-	Code       string     `json:"code"`
-	Message    string     `json:"message"`
-	IncludeMap bool       `json:"includeMap"`
-	MetaData   []dataItem `json:"metaData"`
+	// Code is expected to be Pascal Case. Is a preferable unique string code for an error.
+	Code string `json:"code"`
+	// Message is a string added as the message to the error produced.
+	Message string `json:"message"`
+	// IncludeMap if true adds a map[string]interface{} to the parameters of a constructor so that a genereic map of data can get added to an error constructor parameters list in addition to any specific data defined in MetaData.
+	IncludeMap bool `json:"includeMap"`
+	// MetaData is an array of dataItem that lists specific data that should be added to the error constructor, and added to the errors metadata map.
+	MetaData []dataItem `json:"metaData"`
 }
 
 type jsonErrorFileData struct {
