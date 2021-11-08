85cb9e6b0dd9a67998c076b1b7e439ecaca6d1a6
calvine <calvin.echols@gmail.com>
2021-11-07 21:38:06 -0500

    starting to implement user service

--- service/userservice.go
+++ service/userservice.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 
+	"github.com/calvine/goauth/core/apptelemetry"
 	coreerrors "github.com/calvine/goauth/core/errors"
 	"github.com/calvine/goauth/core/models"
 	repo "github.com/calvine/goauth/core/repositories"
@@ -34,7 +35,20 @@ func (us userService) GetName() string {
 }
 
 func (us userService) GetUserByConfirmedContact(ctx context.Context, logger *zap.Logger, contactPrincipal string, initiator string) (models.User, errors.RichError) {
+	span := apptelemetry.CreateFunctionSpan(ctx, us.GetName(), "GetUserByConfirmedContact")
+	defer span.End()
 	return models.User{}, coreerrors.NewNotImplementedError(true)
+}
+
+func (us userService) RegisterUserAndPrimaryContact(ctx context.Context, logger *zap.Logger, contactPrincipal, contactType string) (models.User, models.Contact, errors.RichError) {
+	span := apptelemetry.CreateFunctionSpan(ctx, us.GetName(), "RegisterUserAndPrimaryContact")
+	defer span.End()
+	// check to see if a user is registered with the confirmed contact info provided
+	// existingUser, existingContact, err := us.userRepo.GetUserAndContactByContact(ctx, contactType, contactPrincipal)
+	// if err != nil {
+	// 	logger.Error("", zap.Error(err))
+	// }
+	return models.User{}, models.Contact{}, coreerrors.NewNotImplementedError(true)
 }
 
 // func (us userService) AddUser(ctx context.Context, logger *zap.Logger, user *models.User, initiator string) errors.RichError {
@@ -46,26 +60,38 @@ func (us userService) GetUserByConfirmedContact(ctx context.Context, logger *zap
 // }
 
 func (us userService) GetUserPrimaryContact(ctx context.Context, logger *zap.Logger, userID string, initiator string) (models.Contact, errors.RichError) {
+	span := apptelemetry.CreateFunctionSpan(ctx, us.GetName(), "GetUserPrimaryContact")
+	defer span.End()
 	return models.Contact{}, coreerrors.NewNotImplementedError(true)
 }
 
 func (us userService) GetUsersContacts(ctx context.Context, logger *zap.Logger, userID string, initiator string) ([]models.Contact, errors.RichError) {
+	span := apptelemetry.CreateFunctionSpan(ctx, us.GetName(), "GetUsersContacts")
+	defer span.End()
 	return nil, coreerrors.NewNotImplementedError(true)
 }
 
 func (us userService) GetUsersConfirmedContacts(ctx context.Context, logger *zap.Logger, userID string, initiator string) ([]models.Contact, errors.RichError) {
+	span := apptelemetry.CreateFunctionSpan(ctx, us.GetName(), "GetUsersConfirmedContacts")
+	defer span.End()
 	return nil, coreerrors.NewNotImplementedError(true)
 }
 
 func (us userService) AddContact(ctx context.Context, logger *zap.Logger, contact *models.Contact, initiator string) errors.RichError {
+	span := apptelemetry.CreateFunctionSpan(ctx, us.GetName(), "AddContact")
+	defer span.End()
 	return coreerrors.NewNotImplementedError(true)
 }
 
 func (us userService) UpdateContact(ctx context.Context, logger *zap.Logger, contact *models.Contact, initiator string) errors.RichError {
+	span := apptelemetry.CreateFunctionSpan(ctx, us.GetName(), "UpdateContact")
+	defer span.End()
 	return coreerrors.NewNotImplementedError(true)
 }
 
 func (us userService) ConfirmContact(ctx context.Context, logger *zap.Logger, confirmationCode string, initiator string) errors.RichError {
+	span := apptelemetry.CreateFunctionSpan(ctx, us.GetName(), "ConfirmContact")
+	defer span.End()
 	return coreerrors.NewNotImplementedError(true)
 }
 
