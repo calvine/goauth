7535b67ecf77a09430bf0b12ebee636b70591bf9
CalvinE <calvin.echols@gmail.com>
2022-01-03 17:24:34 -0500

    added missing telemetry

--- dataaccess/memory/jwtsigningmaterialrepo.go
+++ dataaccess/memory/jwtsigningmaterialrepo.go
@@ -2,8 +2,10 @@ package memory
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	"github.com/calvine/goauth/core/apptelemetry"
 	coreerrors "github.com/calvine/goauth/core/errors"
 	"github.com/calvine/goauth/core/models"
 	repo "github.com/calvine/goauth/core/repositories"
@@ -30,19 +32,27 @@ func (jwtSigningMaterialRepo) GetType() string {
 }
 
 func (jsm jwtSigningMaterialRepo) GetJWTSigningMaterialByKeyID(ctx context.Context, keyID string) (models.JWTSigningMaterial, errors.RichError) {
+	span := apptelemetry.CreateRepoFunctionSpan(ctx, jsm.GetName(), "GetJWTSigningMaterialByKeyID", jsm.GetType())
+	defer span.End()
 	material, ok := (*jsm.material)[keyID]
 	if !ok {
-		fields := make(map[string]interface{})
-		fields["keyID"] = keyID
-		return models.JWTSigningMaterial{}, coreerrors.NewNoJWTSigningMaterialFoundError(keyID, true)
+		rErr := coreerrors.NewNoJWTSigningMaterialFoundError(keyID, true)
+		evtString := fmt.Sprintf("%s: %s", rErr.GetErrorMessage(), keyID)
+		apptelemetry.SetSpanOriginalError(&span, rErr, evtString)
+		return models.JWTSigningMaterial{}, rErr
 	}
 	return material, nil
 }
 
 func (jsm jwtSigningMaterialRepo) AddJWTSigningMaterial(ctx context.Context, jwtSigningMaterial *models.JWTSigningMaterial, createdBy string) errors.RichError {
+	span := apptelemetry.CreateRepoFunctionSpan(ctx, jsm.GetName(), "AddJWTSigningMaterial", jsm.GetType())
+	defer span.End()
 	_, alreadyExists := (*jsm.material)[jwtSigningMaterial.KeyID]
 	if alreadyExists {
-		return coreerrors.NewJWTSigningMaterialKeyIDNotUniqueError(jwtSigningMaterial.KeyID, true)
+		rErr := coreerrors.NewJWTSigningMaterialKeyIDNotUniqueError(jwtSigningMaterial.KeyID, true)
+		evtString := fmt.Sprintf("%s: %s", rErr.GetErrorMessage(), jwtSigningMaterial.KeyID)
+		apptelemetry.SetSpanOriginalError(&span, rErr, evtString)
+		return rErr
 	}
 	jwtSigningMaterial.AuditData.CreatedByID = createdBy
 	jwtSigningMaterial.AuditData.CreatedOnDate = time.Now().UTC()
