853470afa4e2dd68764bd3acc8b89414e80b7572
CalvinE <calvin.echols@gmail.com>
2022-01-03 17:21:02 -0500

    more repo implementation

--- dataaccess/mongo/constants.go
+++ dataaccess/mongo/constants.go
@@ -3,8 +3,9 @@ package mongo
 const (
 	DB_NAME = "goauth"
 
-	USER_COLLECTION     = "users"
-	AUDITLOG_COLLECTION = "auditlog"
+	USER_COLLECTION                 = "users"
+	JWT_SIGNING_MATERIAL_COLLECTION = "jwtsigningmaterial"
+	AUDITLOG_COLLECTION             = "auditlog"
 
 	dataSourceType = "mongo"
 )
--- dataaccess/mongo/jwtsigningmaterialrepo.go
+++ dataaccess/mongo/jwtsigningmaterialrepo.go
@@ -1,1 +1,53 @@
 package mongo
+
+import (
+	"context"
+
+	"github.com/calvine/goauth/core/models"
+	repo "github.com/calvine/goauth/core/repositories"
+	"github.com/calvine/richerror/errors"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type jwtSigningMaterialRepo struct {
+	mongoClient    *mongo.Client
+	dbName         string
+	collectionName string
+}
+
+func NewJWTSigningMaterialRepo(client *mongo.Client) repo.JWTSigningMaterialRepo {
+	return jwtSigningMaterialRepo{client, DB_NAME, JWT_SIGNING_MATERIAL_COLLECTION}
+}
+
+func NewJWTSigningMaterialRepoWithNames(client *mongo.Client, dbName, collectionName string) repo.JWTSigningMaterialRepo {
+	return jwtSigningMaterialRepo{client, dbName, collectionName}
+}
+
+func (jwtSigningMaterialRepo) GetName() string {
+	return "jwtSigningMaterialRepo"
+}
+
+func (jwtSigningMaterialRepo) GetType() string {
+	return dataSourceType
+}
+
+func (jsm jwtSigningMaterialRepo) GetJWTSigningMaterialByKeyID(ctx context.Context, keyID string) (models.JWTSigningMaterial, errors.RichError) {
+	// material, ok := (*jsm.material)[keyID]
+	// if !ok {
+	// 	fields := make(map[string]interface{})
+	// 	fields["keyID"] = keyID
+	// 	return models.JWTSigningMaterial{}, coreerrors.NewNoJWTSigningMaterialFoundError(keyID, true)
+	// }
+	// return material, nil
+}
+
+func (jsm jwtSigningMaterialRepo) AddJWTSigningMaterial(ctx context.Context, jwtSigningMaterial *models.JWTSigningMaterial, createdBy string) errors.RichError {
+	// _, alreadyExists := (*jsm.material)[jwtSigningMaterial.KeyID]
+	// if alreadyExists {
+	// 	return coreerrors.NewJWTSigningMaterialKeyIDNotUniqueError(jwtSigningMaterial.KeyID, true)
+	// }
+	// jwtSigningMaterial.AuditData.CreatedByID = createdBy
+	// jwtSigningMaterial.AuditData.CreatedOnDate = time.Now().UTC()
+	// (*jsm.material)[jwtSigningMaterial.KeyID] = *jwtSigningMaterial
+	// return nil
+}
